94042a7215790abff672457f55b8492ea5230f9d
lcorrea <yancel209@gmail.com>
2022-12-05 10:29:14 -0500

    agregando cambios

---
+++ jobscript_booleano.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Bienvenido/a al planeta $planeta"
+if [$agente = "true"]
+then
+	echo "preparese para la misión agente X, que la fuerza te acompañe."
+else 
+	echo "disfruta del planeta humano/a $nombre, trata de no morir."
+fi
+echo "....."
